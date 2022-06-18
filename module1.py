ff152dfb4eb58aa2e6cb1a0f391ce5c41a527c8e
pulkit <pulkit.patel.pqc@gmail.com>
2022-06-18 21:22:12 +0530

    first commit

---
+++ module1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import findspark
+findspark.init()
+
+
+# In[2]:
+
+
+import pyspark
+from pyspark.sql import SparkSession
+
+
+# In[4]:
+
+
+print(__name__)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
